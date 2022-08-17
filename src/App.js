edd3dec9e9ae032257f6e0d7d90d422c08834c0d
Arpan Barua <arpanctg7@gmail.com>
2022-08-17 13:13:19 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {useForm} from "react-hook-form";
 import './App.css';
 
 function App() {
+  const {register, handleSubmit, formState: { errors }, reset} = useForm();
+  const [userInfo, setUserInfo] = useState();
+  const onSubmit = (data)=>{
+    setUserInfo(data);
+    reset();
+    console.log(data);
+    console.log(errors)
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>User Name</label>
+        <input type="text" placeholder="Username" {...register("username",{required:true})}/>
+      </div>
+      {errors.username && <p>Username is required</p>}
+      <div>
+        <label>Email</label>
+        <input type="email" placeholder="Email" {...register("email",{required:true, pattern:/^\S+@\S+\.\S+$/})}/>
+      </div>
+      {errors.email && <p>Email is required</p>}
+      <div>
+        <label>Password</label>
+        <input type="password" placeholder="Password" {...register("password",{required:true, minLength:4, maxLength:8})}/>
+      </div>
+     {errors.password && <p>Password is required</p>}
+      
+      <input type="submit" />
+      
+    </form>  
     </div>
   );
 }
